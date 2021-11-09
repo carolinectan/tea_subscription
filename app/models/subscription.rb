475ebc09f93a636c6057e52d0254f62d40808cb7
Caroline Tan <carolinectan3@gmail.com>
2021-11-09 14:50:07 -0500

    Add relationships and validations to models

---
+++ app/models/subscription.rb
@@ -0,0 +1,9 @@
+class Subscription < ApplicationRecord
+  belongs_to :customer
+  belongs_to :tea
+
+  validates :title, presence: true
+  validates :price, presence: true, numericality: true
+  enum status: { 'active' => 0, 'cancelled' => 1}
+  enum frequency: { 'monthly' => 0, 'quarterly' => 1}
+end
