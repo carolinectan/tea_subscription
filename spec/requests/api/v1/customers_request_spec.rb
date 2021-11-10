d4139863d4377de88b54a4164a8a70966fc0721f
Caroline Tan <carolinectan3@gmail.com>
2021-11-10 00:22:31 -0500

    Add customers request spec

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -2,10 +2,38 @@ require 'rails_helper'
 
 describe 'customers api' do
   it 'sends a list of customers' do
-    create_list(:customer, 3)
+    Customer.destroy_all
+
+    customer1 = create(:customer, first_name: 'Jacob',
+                                  last_name: 'McGuire',
+                                  email: 'brewer512@gmail.com',
+                                  address: '33 Lilah Ln, Denver, CO 80111')
+    create_list(:customer, 2)
 
     get '/api/v1/customers'
 
     expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:data].length).to eq(3)
+
+    expect(json[:data].first[:id]).to be_a String
+    expect(json[:data].first[:id]).to eq(customer1.id.to_s)
+
+    expect(json[:data].first[:type]).to be_a String
+    expect(json[:data].first[:type]).to eq('customer')
+
+    expect(json[:data].first[:attributes]).to be_a Hash
+    expect(json[:data].first[:attributes].length).to eq(4)
+
+    expect(json[:data].first[:attributes][:first_name]).to be_a String
+    expect(json[:data].first[:attributes][:first_name]).to eq('Jacob')
+    expect(json[:data].first[:attributes][:last_name]).to be_a String
+    expect(json[:data].first[:attributes][:last_name]).to eq('McGuire')
+    expect(json[:data].first[:attributes][:email]).to be_a String
+    expect(json[:data].first[:attributes][:email]).to eq('brewer512@gmail.com')
+    expect(json[:data].first[:attributes][:address]).to be_a String
+    expect(json[:data].first[:attributes][:address]).to eq('33 Lilah Ln, Denver, CO 80111')
   end
 end
