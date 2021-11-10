5ea7347e36955e4acc3975d91651eb24e3025ec8
Caroline Tan <carolinectan3@gmail.com>
2021-11-10 12:55:47 -0500

    Add customer subscriptions route to index action

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers, only: [:index]
+      resources :customer_subscriptions, only: [:index]
     end
   end
 end
