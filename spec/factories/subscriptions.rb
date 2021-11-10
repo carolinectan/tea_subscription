a94cdff4890564f75c83edfe7b69aab9b83eb327
Caroline Tan <carolinectan3@gmail.com>
2021-11-10 13:31:20 -0500

    Add Rubocop styling

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     customer
     tea
     title { Faker::Marketing.buzzwords }
-    price { [9.99, 10.99, 11.99, 12.99, 13.99, 14.99, 15.99].sample  }
+    price { [9.99, 10.99, 11.99, 12.99, 13.99, 14.99, 15.99].sample }
     status { ['active', 'cancelled'].sample }
     frequency { ['monthly', 'quarterly'].sample }
   end
