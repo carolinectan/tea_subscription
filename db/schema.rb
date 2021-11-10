8dcd197d3adf1384194a83ebd8a3513ac839c1a4
Caroline Tan <carolinectan3@gmail.com>
2021-11-09 19:07:27 -0500

    Add default of active to subscriptions status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_192437) do
+ActiveRecord::Schema.define(version: 2021_11_09_235849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_11_09_192437) do
     t.bigint "tea_id"
     t.string "title"
     t.decimal "price"
-    t.integer "status"
+    t.integer "status", default: 0, null: false
     t.integer "frequency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
