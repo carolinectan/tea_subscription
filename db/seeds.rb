37ad8eb975872d765cd4ab1b1ed8987771cd5f6e
Caroline Tan <carolinectan3@gmail.com>
2021-11-10 00:38:41 -0500

    Seed database with teas and subscriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,4 +17,20 @@ customer1 = create(:customer, first_name: 'Amanda')
 customer2 = create(:customer, first_name: 'Jacob')
 customer3 = create(:customer, first_name: 'Carina')
 customer4 = create(:customer, first_name: 'Caroline')
-customer5 = create(:customer, first_name: 'Brian')
+customer5 = create(:customer, first_name: 'Brian') # no subscription
+
+# Teas
+tea1 = create(:tea, title: 'Sleepy time')
+tea2 = create(:tea, title: 'Positive energy')
+tea3 = create(:tea, title: 'Balance')
+tea4 = create(:tea, title: 'Calming energy')
+tea5 = create(:tea, title: 'Energizing')
+
+# Subscriptions
+subscription1 = create(:subscription, tea: tea1, customer: customer1)
+subscription2 = create(:subscription, tea: tea2, customer: customer2)
+subscription3 = create(:subscription, tea: tea3, customer: customer2)
+subscription4 = create(:subscription, tea: tea4, customer: customer3)
+subscription5 = create(:subscription, tea: tea5, customer: customer3)
+subscription6 = create(:subscription, tea: tea4, customer: customer3)
+subscription7 = create(:subscription, tea: tea5, customer: customer4)
