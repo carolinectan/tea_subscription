8059abb65a5d440a63d3897ba7088df3793c683b
Caroline Tan <carolinectan3@gmail.com>
2021-11-11 18:55:14 -0500

    Add create action, add subscription params method, and rename serializer

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -4,6 +4,22 @@ class Api::V1::SubscriptionsController < ApplicationController
   def index
     customer_subscriptions = Customer.find(params[:customer_id]).subscriptions
 
-    render json: CustomerSubscriptionsSerializer.new(customer_subscriptions), status: :ok
+    render json: SubscriptionSerializer.new(customer_subscriptions), status: :ok
+  end
+
+  def create
+    subscription = Subscription.new(subscription_params)
+
+    if subscription.save
+      render json: SubscriptionSerializer.new(subscription), status: :created
+    else
+      render json: {}, status: :bad_request
+    end
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:customer_id, :tea_id, :title, :price, :frequency)
   end
 end
