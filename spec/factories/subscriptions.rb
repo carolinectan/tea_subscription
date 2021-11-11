db7a03061ec746480974f381dec3ca47f9959a10
Caroline Tan <carolinectan3@gmail.com>
2021-11-11 18:53:37 -0500

    Change Faker category for subscription title

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :subscription do
     customer
     tea
-    title { Faker::Marketing.buzzwords }
+    title { Faker::Movies::HarryPotter.spell }
     price { [9.99, 10.99, 11.99, 12.99, 13.99, 14.99, 15.99].sample }
     status { ['active', 'cancelled'].sample }
     frequency { ['monthly', 'quarterly'].sample }
