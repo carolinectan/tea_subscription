139e53e44eda1c141e5ac7810d7b77dc971b3895
Caroline Tan <carolinectan3@gmail.com>
2021-11-11 18:08:23 -0500

    Add route to customers controller show action

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :customers, only: [:index]
+      resources :customers, only: [:index, :show]
       resources :customer_subscriptions, only: [:index]
     end
   end
