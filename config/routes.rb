7e441de3b67b5d7592628c9a889dc54d40598b73
Caroline Tan <carolinectan3@gmail.com>
2021-11-11 23:19:23 -0500

    Add subscriptions update route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers, only: [:index, :show] do
-        resources :subscriptions, only: [:index, :create]
+        resources :subscriptions, only: [:index, :create, :update]
       end
     end
   end
