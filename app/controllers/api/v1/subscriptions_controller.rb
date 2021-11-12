e23d9480f1a27c317ff583db3689ddf898a39f69
Caroline Tan <carolinectan3@gmail.com>
2021-11-12 01:48:55 -0500

    Refactor index, create, and update actions for sad path handling

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -5,23 +5,42 @@ class Api::V1::SubscriptionsController < ApplicationController
     customer_subscriptions = Customer.find(params[:customer_id]).subscriptions
 
     render json: SubscriptionSerializer.new(customer_subscriptions), status: :ok
+
+  rescue ActiveRecord::RecordNotFound
+    invalid_credentials
   end
 
   def create
-    subscription = Subscription.new(subscription_params)
+    customer = Customer.find(params[:customer_id])
+    tea = Tea.find(params[:tea_id])
 
-    if subscription.save
-      render json: SubscriptionSerializer.new(subscription), status: :created
-    else
-      render json: {}, status: :bad_request
+    if !params[:title] || !params[:price] || !params[:frequency]
+      render json: {
+        message: 'Your request could not be completed.',
+        errors: ['All attributes are required.']
+      }, status: :bad_request
+    elsif
+      subscription = Subscription.new(subscription_params)
+
+      if subscription.save
+        render json: SubscriptionSerializer.new(subscription), status: :created
+      else
+        render json: {}, status: :bad_request
+      end
     end
+
+  rescue ActiveRecord::RecordNotFound
+    invalid_credentials
   end
 
   def update
-    subscription = Subscription.find_by(subscription_params)
+    subscription = Subscription.find(params[:id])
     subscription.update!(status: params[:status])
 
     render json: SubscriptionSerializer.new(subscription), status: :ok
+
+  rescue ActiveRecord::RecordNotFound
+    invalid_credentials
   end
 
   private
