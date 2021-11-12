ca8e65a34bed654ff580da984f40b0f3fc3ddb79
Caroline Tan <carolinectan3@gmail.com>
2021-11-11 23:19:52 -0500

    Remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers, only: [:index, :show] do
